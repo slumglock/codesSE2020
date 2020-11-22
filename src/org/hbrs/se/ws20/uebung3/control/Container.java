5d58df69b8a39be2bf09e0446fdf8c6462584814
slumglock <leon.gl@web.de>
2020-11-22 14:57:08 +0100

    Copy für Ü3 + Singleton Pattern für CR1

---
+++ src/org/hbrs/se/ws20/uebung3/control/Container.java
@@ -0,0 +1,91 @@
+package org.hbrs.se.ws20.uebung3.control;
+
+
+import org.hbrs.se.ws20.uebung2.Member;
+import org.hbrs.se.ws20.uebung2.control.ContainerException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Container {
+
+    // CR1: Nur 1 Container Objekt pro Laufzeit
+
+    private static Container instance; //instance = null
+
+    private Container() {}
+
+    public static Container getInstance(){
+        if(instance == null) {
+            instance = new Container();
+        }
+        return instance;
+    }
+
+    public List<Member> aList = new ArrayList<>();
+
+    public void addMember(Member member) throws ContainerException {
+
+
+        if (contains(member)){
+            ContainerException exception = new ContainerException();
+            exception.setExceptionID(member.getID());
+            throw exception;
+
+        }
+        aList.add(member);
+
+    }
+
+    public boolean contains(Member member){
+        Integer id = member.getID();
+        for(Member x : aList) {
+            if(member.getID().equals(x.getID())){
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+
+    public String deleteMember(Integer id) {
+        Member x = getMember(id);
+        if (x == null) {
+            return "Kein Member mit der ID (" + id + ") ist in der Liste enthalten.";
+
+        } else {
+            aList.remove(x);
+            return "Der Member mit der ID (" + id + ") wurde erfolgreich gelöscht.";
+        }
+    }
+
+
+
+
+
+    private Member getMember(Integer id) {
+        for(Member x:aList){
+            if(id.equals(x.getID())){
+                return x;
+            }
+        }
+        return null;
+
+
+    }
+
+    public void dump(){
+        System.out.print("Alle IDs der aktuell gespeicherten Obejkte im Container:\n");
+        for(Member x:aList){
+            System.out.print(x.toString());
+        }
+    }
+
+    public int size(){
+
+        return aList.size();
+
+    }
+
+}
